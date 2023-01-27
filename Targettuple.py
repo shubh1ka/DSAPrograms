ff8a9ca54014dc1c4582ecf7c25340514a89e2bb
Shubhika Singh <93629980+shubh1ka@users.noreply.github.com>
2023-01-27 17:40:19 +0530

    Add files via upload

---
+++ Targettuple.py
@@ -0,0 +1,12 @@
+def res(l1,n1):
+    for i in range(0,len(l1)):
+        for j in range(1,len(l1)):
+            if l1[i]+l1[j]==n1:
+                return(l1[i],l1[j])
+
+
+if __name__ == "__main__":
+    l1=list(map(int,input().split()))
+    n1=input()
+    print(res(l1,n1))
+
